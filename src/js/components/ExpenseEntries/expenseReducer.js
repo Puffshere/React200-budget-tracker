44b133dcd89c84a751980d1b18a0c3b00757f1e0
Shawn Taylor <shawncurtistaylor@gmail.com>
2019-10-04 15:03:42 -0700

    final commit I believe

--- src/js/components/ExpenseEntries/expenseReducer.js
+++ src/js/components/ExpenseEntries/expenseReducer.js
@@ -1,8 +1,39 @@
 const defaultState = {
     description: '',
+    amount: '',
     lineItems: []
 };
 
-export default function ExpenseReducer (state = defaultState, action) {
-    return state;
+export default function ExpenseReducer(state = defaultState, action) {
+    const { type, payload } = action;
+
+    switch (type) {
+        case 'UPDATE_EXPENSE_DESCRIPTION': {
+            return {
+                ...state,
+                description: payload.description
+            };
+        }
+        case 'UPDATE_EXPENSE_AMOUNT': {
+            return {
+                ...state,
+                amount: payload.amount
+            };
+        }
+        case 'ADD_EXPENSE': {
+            const { description, amount } = action.payload;
+            return {
+                description: '',
+                action: '',
+                lineItems: [
+                    ...state.lineItems,
+                    { description, amount }
+                ]
+            };
+        }
+
+        default: {
+            return state;
+        }
+    }
 }
